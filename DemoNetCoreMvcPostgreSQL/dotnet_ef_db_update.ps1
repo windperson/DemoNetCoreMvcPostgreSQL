d15f72c7657573ebd585e195067025fad22382b7
Isak Pao <windperson@gmail.com>
2021-07-22 19:34:21 +0800

    TODO: use "dotnet-ef" via local tool instead of global tools

--- DemoNetCoreMvcPostgreSQL/dotnet_ef_db_update.ps1
+++ DemoNetCoreMvcPostgreSQL/dotnet_ef_db_update.ps1
@@ -2,6 +2,10 @@ $checkApplyedFunc = [Func[PSCustomObject, bool]] { $args[0].applied -eq $false }
 
 $previous = dotnet ef migrations list --json --prefix-output | Where-Object { $_.StartsWith('data:') } | ForEach-Object { $_.Substring(5) } | ConvertFrom-Json;
 
+if ( -not $? ) {
+  throw "get db migration infos from target db failed!"
+}
+
 if ( [Linq.Enumerable]::Any([PSCustomObject[]]$previous, $checkApplyedFunc) ) {
   Write-Host "New DB migrations detected, apply `"dotnet ef database update`"";
   # Do db migration update
--- Readme.md
+++ Readme.md
@@ -1,9 +1,9 @@
 # ASP.NET Core MVC PostgreSQL DB demo
 
-1. Spin up DB by invoking command line in Powershell which inside the `docker_postgis` folder:
+1. Spin up DB by invoking command line in Powershell, switching to the `docker_postgis` folder, then invoke:
   
     ```powershell
-    .\dev_start.ps1 -compose_proj DemoNetCoreMvc -gis_port 5433
+    ./dev_start.ps1 -compose_proj DemoNetCoreMvc -gis_db demo_db -gis_port 5433
     ```
   
 2. Run `dotnet run` inside the `DemoNetCoreMvcPostgreSQL` folder.
