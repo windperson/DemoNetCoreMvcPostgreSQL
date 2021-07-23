aa48591731c0686a8e200ca9a5b7a0eb87e8eb17
Isak Pao <windperson@gmail.com>
2021-07-23 19:51:48 +0800

    TODO: add cli switch to generate DB migration script

---
+++ DB_script/Readme.md
@@ -0,0 +1,3 @@
+# DB_script
+
+DB migration script file will be generated here.
--- DemoNetCoreMvcPostgreSQL/dotnet_ef_db_update.ps1
+++ DemoNetCoreMvcPostgreSQL/dotnet_ef_db_update.ps1
@@ -1,19 +1,48 @@
+<#
+.SYNOPSIS
+    Auto apply EF Core migration changes on target database and/or generate DB update script
+#>
+param (
+  # (Alias: conn)
+  # 
+  # DB connection string to the target DB, default empty. 
+  [Alias("conn")]
+  [Parameter(Position = 0)]
+  [String] $DB_Connection = [string]::Empty
+)
+
+Write-Debug "`$DB_Connection='$DB_Connection'";
+
+# Lambda function for using in following Linq Any() operation
 $checkApplyedFunc = [Func[PSCustomObject, bool]] { $args[0].applied -eq $false }
 
-$previous = dotnet ef migrations list --json --prefix-output | Where-Object { $_.StartsWith('data:') } | ForEach-Object { $_.Substring(5) } | ConvertFrom-Json;
-
-if ( -not $? ) {
-  throw "get db migration infos from target db failed!"
+if ( -not $DB_Connection ) {
+  $migrate_list = dotnet dotnet-ef migrations list --json --prefix-output | Where-Object { $_.StartsWith('data:') } | ForEach-Object { $_.Substring(5) } | ConvertFrom-Json;
+}
+else {
+  Write-Verbose "Use `$DB_Connection='$DB_Connection'";
+  $migrate_list = dotnet dotnet-ef migrations list --connection "$DB_Connection" --json --prefix-output | Where-Object { $_.StartsWith('data:') } | ForEach-Object { $_.Substring(5) } | ConvertFrom-Json;
 }
 
-if ( [Linq.Enumerable]::Any([PSCustomObject[]]$previous, $checkApplyedFunc) ) {
-  Write-Host "New DB migrations detected, apply `"dotnet ef database update`"";
-  # Do db migration update
-  $execStr = "dotnet ef database update --verbose"
-  Write-Verbose "$execStr"
-  Invoke-Expression "$execStr";
+if ( -not $? ) {
+  throw "Get DB migration records from target DB failed, be sure to install dotnet ef cli tool and check DB connection correctness."
+}
 
+if ( [Linq.Enumerable]::Any([PSCustomObject[]]$migrate_list, $checkApplyedFunc) ) {
+  # Do db migration update
+  Write-Verbose "New DB migrations detected, apply `"dotnet ef database update`":";  
+  PrintAndExecuteStr -exec "dotnet dotnet-ef database update --verbose"
+  Write-Verbose "Create DB migration script:"
+  PrintAndExecuteStr -exec "dotnet dotnet-ef migrations script --idempotent --output ../DB_script/update.sql --verbose"
   return;
 }
 
-Write-Host "No DB migrations changes detected, exit.";
+Write-Information "No DB migrations changes detected, exit.";
+
+function PrintAndExecuteStr {
+  param (
+    [String] $exec
+  )
+  Write-Verbose "$exec"
+  Invoke-Expression "$exec";
+}
